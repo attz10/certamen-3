7691b13406c5362cdc232126506791405680540d
attz10 <94582755+attz10@users.noreply.github.com>
2023-06-19 18:08:09 -0400

    agregue iconos a la tabla de admins

--- proyecto/resources/views/administradores/index.blade.php
+++ proyecto/resources/views/administradores/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('principal')
     <div class="row">
+        <div class="col-3"></div>
         <div class="col-6">
             <div class="col-12 col-lg-8 order-last order-lg-first">
                 <table class="table table-bordered table-striped table-hover">
@@ -12,7 +13,7 @@
                             <th>Nombre</th>
                             <th>Apellido</th>
                             <th>Tipo de perfil</th>
-                            <th colspan="2">Acciones</th>
+                            <th colspan="3">Acciones</th>
                             
                         </tr>
                     </thead>
@@ -31,12 +32,13 @@
                                 @endforeach
                             </td>
                             <td>
-                                <a href="" class="btn btn-info">Editar</a>
+                                <a href="" class="btn btn-info"><span class="material-icons">edit</span></a>
                             </td>
                             <td>
                                 @foreach ($perfiles as $perfil)
                                     @if (($perfil->id==$cuenta->perfil_id)&&($perfil->nombre=='artista'))
-                                    <a href="" class="btn btn-warning">Borrar</a>
+                                    <a href="" class="btn btn-warning"><span class="material-icons">delete</span></a>
+                                    <td><a href="" class="btn btn-warning"><span class="material-icons">image</span></a></td>
                                     @endif
                                 @endforeach
                             </td>
@@ -46,5 +48,6 @@
                 </table>
             </div>
         </div>
+        <div class="col-3"></div>
     </div>
 @endsection
